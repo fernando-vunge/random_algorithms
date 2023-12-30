a87b2e36bbc62c050677a09830f13878bb8ddb6d
Fernando Julio Vunge <developervunge@gmail.com>
2023-12-30 11:58:31 +0000

    Update

--- largest_product.py
+++ largest_product.py
@@ -2,6 +2,7 @@ def highest_produ(matrix):
     horizontal_product = 1
     vertical_product = 1
     highest_product = 1
+    qtd_zeros = 0
 
     for c in range(0,len(matrix[0])):
         for row in matrix:
@@ -15,9 +16,17 @@ def highest_produ(matrix):
         if(vertical_product >= highest_product):
             highest_product = vertical_product
         vertical_product = 1
+    
+    for row in matrix:
+        for columm in row:
+            if(columm == 0):
+                qtd_zeros += 1
+                if(qtd_zeros >= len(row)) :
+                    return 0
+        qtd_zeros  = 0
     return highest_product
 
 
-matrix = [[2, 1, 4, 1], [0, 4, 8, 1], [1, 0, 10, 0]]
+matrix = [[2, 1, 4, 1], [0, 2, 0, 0], [1, 0, 10, 0]]
 result = highest_produ(matrix)
 print(result)
